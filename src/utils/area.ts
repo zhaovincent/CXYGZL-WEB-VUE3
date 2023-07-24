84c6ad4d6c61946e8ba925b014b0b016d5cbd373
zhaohuijun <zhaohuijun2021>
2023-07-25 01:19:15 +0800

    feat: 1、地区值添加code

--- src/utils/area.ts
+++ src/utils/area.ts
@@ -6,19 +6,23 @@ export function getAreaValue(dataList, t){
 
 	var nameObj = {}
 	var name = '';
+	var code = '';
 
 	if (t[0]) {
 		let e = dataList.value.filter(res => res.code === t[0])[0];
 		nameObj[0] = e.name;
 		name = name + e.name;
+		code=t[0]
 		if (t[1]) {
 			let f = e.children.filter(res => res.code === t[1])[0];
+			code=t[1]
 
 			nameObj[1] = f.name;
 			name = name + f.name;
 
 			if (t[2]) {
 				let g = f.children.filter(res => res.code === t[2])[0];
+				code=t[2]
 
 				nameObj[2] = g.name;
 				name = name + g.name;
@@ -28,7 +32,7 @@ export function getAreaValue(dataList, t){
 	}
 
 	return {
-		value: t, nameObj: nameObj, name: name
+		value: t, nameObj: nameObj, name: name,code:code
 	}
 }
 
