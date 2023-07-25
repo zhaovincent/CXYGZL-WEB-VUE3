1b5d5246ced87189e17b9d5ac1c590afa39b9180
zhaohuijun <18766289420@163.com>
2023-07-25 09:54:17 +0800

    feat: 完成地区表单

--- src/utils/area.ts
+++ src/utils/area.ts
@@ -1,30 +1,32 @@
 export function getAreaValue(dataList, t){
-	console.log(t)
+
 	if(!t){
 		return {}
 	}
 
-	var nameObj = {}
+	var nameObj =[]
 	var name = '';
 	var code = '';
 
 	if (t[0]) {
 		let e = dataList.value.filter(res => res.code === t[0])[0];
-		nameObj[0] = e.name;
+		nameObj.push( e.name);
 		name = name + e.name;
 		code=t[0]
 		if (t[1]) {
 			let f = e.children.filter(res => res.code === t[1])[0];
 			code=t[1]
 
-			nameObj[1] = f.name;
+			nameObj.push( f.name);
+
 			name = name + f.name;
 
 			if (t[2]) {
 				let g = f.children.filter(res => res.code === t[2])[0];
 				code=t[2]
 
-				nameObj[2] = g.name;
+				nameObj.push( g.name);
+
 				name = name + g.name;
 
 			}
