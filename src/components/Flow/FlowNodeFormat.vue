f477fdd68dc54054c1ee23fe20de00e645e36da4
zhaohuijun <18766289420@163.com>
2023-08-09 16:53:53 +0800

    feat: 优化任务待办处理按钮显示

--- src/components/Flow/FlowNodeFormat.vue
+++ src/components/Flow/FlowNodeFormat.vue
@@ -3,7 +3,6 @@ import {ref, getCurrentInstance, onMounted, watch} from 'vue'
 import selectShow from "@/views/flow/workflow/components/dialog/selectAndShow.vue";
 
 
-
 let props = defineProps({
 
 	nodeUser: {
@@ -15,9 +14,9 @@ let props = defineProps({
 		type: Array,
 		dafault: () => []
 	},
-	disableSelect:{
-		type:Boolean,
-			default:false
+	disableSelect: {
+		type: Boolean,
+		default: false
 	}
 
 
@@ -25,7 +24,7 @@ let props = defineProps({
 const {proxy} = getCurrentInstance();
 
 
-import {Loading, Finished, Refresh,Clock,CircleCloseFilled} from "@element-plus/icons-vue";
+import {Loading, Finished, Refresh, Clock, CircleCloseFilled} from "@element-plus/icons-vue";
 
 
 </script>
@@ -35,9 +34,9 @@ import {Loading, Finished, Refresh,Clock,CircleCloseFilled} from "@element-plus/
 
 		<el-timeline :reverse="false">
 			<el-timeline-item v-for="(node, index) in row" :key="index" size="large"
-												:color="node.status!=2?(
-        node.status==1?'green':(node.status==3?'red':'grey')
-      ):'blue'"
+												:type="node.status!=2?(
+        node.status==1?'success':(node.status==3?'danger':'info')
+      ):'primary'"
 												:icon="node.status==2?Finished:(node.status==1?Loading:(node.status==3?CircleCloseFilled:Clock))"
 			>
 				<template v-if="node.selectUser&&(!nodeUser[node.id]||nodeUser[node.id]?.length==0)">
@@ -46,29 +45,45 @@ import {Loading, Finished, Refresh,Clock,CircleCloseFilled} from "@element-plus/
 					</p>
 				</template>
 				<template v-else>
-					<p>{{ node.name }}
-						<template v-if="node.placeholder&&node.placeholder.length>0">[{{ node.placeholder }}]</template>
-					</p>
+<!--					<p>{{ node.name }}-->
+<!--					</p>-->
+						<div style="display: flex;flex-direction: row">
+								<div class="f21">{{node.name}}</div>
+								<div class="f22">{{node.showTimeStr}}</div>
+						</div>
+			<div v-if="node.placeholder&&node.placeholder.length>0">[{{ node.placeholder }}]</div>
+
 				</template>
 
 
 				<!--					渲染用户头像列表-->
 				<div v-if="node.userVoList&&node.userVoList.length>0" style="display: flex;flex-direction: row;flex-wrap: wrap">
 					<div class="box-card" v-for="(item1,index1) in node.userVoList" :key="index1"
-							 style="margin-bottom: 10px;border: 0px solid red;width: 40px;text-align: center">
+							 style="margin-bottom: 10px;border: 0px solid red;width: 60px;text-align: center">
 
 						<div class="node-show">
 							<div style="overflow: hidden;">
 								<div class="d1">
-									<div>
-										<el-avatar shape="square" :size="30" :src="item1.avatar"></el-avatar>
+
+									<div style="height: 60px;padding-top: 20px;">
+										<el-badge :value="12" is-dot
+															:type="item1.status==1?'success':(item1.status==2?'primary':(item1.status==3?'danger':'info'))"
+															class="item">
+
+											<el-avatar shape="square" :size="30" :src="item1.avatar"></el-avatar>
+
+										</el-badge>
 
 									</div>
+
+
 									<div
-											style="font-size: 8px;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;text-align: center">
+											style="font-size: 8px;text-align: center">
 										{{ item1.name }}
 									</div>
+
 								</div>
+
 
 							</div>
 
@@ -81,29 +96,30 @@ import {Loading, Finished, Refresh,Clock,CircleCloseFilled} from "@element-plus/
 				</div>
 				<!--					渲染审批评论-->
 				<template v-for="(item1,index1) in node.userVoList" :key="index1">
-						<template v-if="item1.approveDesc?.length>0">
+					<template v-if="item1.approveDesc?.length>0">
 
-					<div style="display: flex;flex-direction: row">
-						<div style="width: 40px;text-align: center">
+						<div style="display: flex;flex-direction: row">
+							<div class="f1">
 
-							<el-avatar shape="square" :size="30" :src="item1.avatar"></el-avatar>
+								<el-avatar shape="square" :size="30" :src="item1.avatar"></el-avatar>
 
+
+							</div>
+							<div class="f2">
+
+								{{ item1.name }}
+							</div>
+
+							<div style="height:40px;line-height: 40px;font-size: 10px;" class="f4">
+								{{ item1.showTimeStr }}
+							</div>
 
 						</div>
-						<div style="height:40px;line-height: 40px;font-size: 10px;">
-
-							{{ item1.name }}
+						<div class="box-card"
+								 style="margin-bottom: 10px;padding: 5px;background-color:var(--el-fill-color-light)">
+							{{ item1.approveDesc }}
 						</div>
-						<div style="height:40px;line-height: 40px;font-size: 10px;">
-							(添加了评论)
-						</div>
-
-					</div>
-					<div class="box-card"
-							 style="margin-bottom: 10px;padding: 5px;background-color:var(--el-fill-color-light)">
-						{{ item1.approveDesc }}
-					</div>
-			</template>
+					</template>
 
 				</template>
 
@@ -112,7 +128,8 @@ import {Loading, Finished, Refresh,Clock,CircleCloseFilled} from "@element-plus/
 				<template v-if="node.selectUser">
 
 
-					<select-show :disabled="disableSelect" v-model:orgList="nodeUser[node.id]" type="user" :multiple="node.multiple"></select-show>
+					<select-show :disabled="disableSelect" v-model:orgList="nodeUser[node.id]" type="user"
+											 :multiple="node.multiple"></select-show>
 
 
 				</template>
@@ -139,5 +156,37 @@ import {Loading, Finished, Refresh,Clock,CircleCloseFilled} from "@element-plus/
 </template>
 
 <style scoped lang="less">
+.f1 {
+	width: 40px;
+	text-align: center;
+}
 
+.f2 {
+	height: 40px;
+	line-height: 40px;
+	font-size: 10px;
+	margin-left: 5px;
+	width: 80px;
+}
+
+.f4 {
+	width: calc(100% - 125px);
+	text-align: right;
+}
+
+.f00 {
+	width: 10px;
+	height: 10px;
+	display: inline-block;
+	background-color: green;
+	position: fixed;
+	top: 62px;
+
+}
+.f21{
+	width: calc(100% - 50px);
+}
+.f22{
+	width: 50px;
+}
 </style>
