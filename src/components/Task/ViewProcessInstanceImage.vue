798b02a1cda28a5609ff44a06a837e789c7dab59
zhaohuijun <zhaohuijun2021>
2023-08-10 23:52:02 +0800

    feat: 显示流程图

--- src/components/Task/ViewProcessInstanceImage.vue
+++ src/components/Task/ViewProcessInstanceImage.vue
@@ -1,110 +1,61 @@
 <script setup lang="ts">
 import LogicFlow from '@logicflow/core';
 import '@logicflow/core/dist/style/index.css';
+import {NodeResize} from '@logicflow/extension';
+import '@logicflow/extension/lib/style/index.css'
+
+LogicFlow.use(NodeResize);
 import {showImage} from '@/api/task/index'
 
 import {defineExpose} from "vue";
 
 const dialogVisible = ref(false);
 
+const lf =ref();
+const zoomFunc=(v)=>{
+	lf.value.zoom(v)
+}
+
 const view = (row) => {
+	dialogVisible.value = true
 
-	showImage(row.processInstanceId).then(res=>{
-	  imgBase64.value='data:image/png;base64,'+res.data
+	showImage(row.processInstanceId).then(res => {
+		// imgBase64.value = 'data:image/png;base64,' + res.data.base64
+		nodeData.value = res.data;
 
 
-	  dialogVisible.value=true
+		  lf.value = new LogicFlow({
+			container: document.querySelector('#container'),
+			// stopScrollGraph: true,
+			// stopZoomGraph: true,
+			width: screen.width*0.8-100,
+			height: screen.height*0.6,
+			grid: true,
+			isSilentMode: true
+
+		});
+	  lf.value.setTheme({
+		  nodeText: { // 节点文本样式
+			  fontSize: 10,
+			  color: 'green'
+		  },
+
+		  rect: {
+			  width: 50,
+			  height: 40,
+			  radius: 6
+		  },
+	  })
+		lf.value.render(nodeData.value);
+
 
 	})
-	dialogVisible.value = true
-	setTimeout(() => {
-		const lf = new LogicFlow({
-			container: document.querySelector('#container'),
-			stopScrollGraph: true,
-			stopZoomGraph: true,
-			width: 800,
-			height: 500,
-			grid: false,
-			isSilentMode: true
-		});
-		lf.setTheme({
-			circle: {
-				r: 30,
-		  stroke:'red'
-			}
-		})
-		lf.render(data);
-	}, 2000)
 
 
 }
 defineExpose({view});
 
-
-const imgBase64 = ref()
-const data = {
-	// 节点
-	nodes: [
-		{
-			id: 1,
-			type: 'circle',
-			text: '开始',
-			x: 100,
-			y: 150,
-		},
-		{
-			id: 2,
-			type: 'rect',
-			x: 300,
-			y: 150,
-			text: '你好',
-		},
-		{
-			id: 3,
-			type: 'diamond',
-			x: 500,
-			y: 150,
-			text: '条件分支',
-		},
-		{
-			id: 4,
-			type: 'rect',
-			x: 700,
-			y: 100,
-			text: '审批人1',
-		},
-		{
-			id: 5,
-			type: 'rect',
-			x: 700,
-			y: 200,
-			text: '审批人2',
-		},
-	],
-	// 边
-	edges: [
-		{
-			type: 'polyline',
-			sourceNodeId: 1,
-			targetNodeId: 2,
-		},
-		{
-			type: 'polyline',
-			sourceNodeId: 2,
-			targetNodeId: 3,
-		},
-		{
-			type: 'polyline',
-			sourceNodeId: 3,
-			targetNodeId: 4,
-		},
-		{
-			type: 'polyline',
-			sourceNodeId: 3,
-			targetNodeId: 5,
-		},
-	],
-};
+const nodeData = ref({nodes: [], edges: []});
 
 
 </script>
@@ -114,9 +65,10 @@ const data = {
 		<el-dialog
 				v-model="dialogVisible"
 				title="查看流程图"
-				width="850px"
+				width="80%"
 		>
-				  <img :src="imgBase64" style="width: 100%;"/>
+				<el-button @click="zoomFunc(false)">缩小</el-button>
+				<el-button @click="zoomFunc(true)">放大</el-button>
 			<div style="display: block" id="container"></div>
 		</el-dialog>
 	</div>
