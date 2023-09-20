f1fc8f7cd537c2986b45fca2af212004e5cca6e3
zhaohuijun <18766289420@163.com>
2023-09-20 15:04:33 +0800

    feat:  表格合并

--- src/views/cxygzl/views/flow/flowdata.vue
+++ src/views/cxygzl/views/flow/flowdata.vue
@@ -7,6 +7,10 @@ import {
 	queryFlowDataList
 } from "../../api/flow";
 
+import {
+	getLcmArray, isNotBlank
+} from "../../utils/objutil";
+
 import {RoleQuery} from "../../api/role/types";
 
 
@@ -22,34 +26,132 @@ const roleList = ref([]);
 const headList = ref([]);
 
 
-
-
-import { LocationQuery, LocationQueryValue, useRoute } from "vue-router";
+import {LocationQuery, LocationQueryValue, useRoute} from "vue-router";
 
 const route = useRoute();
+
+const spanArray = ref([]);
 
 /**
  * 查询
  */
 function handleQuery() {
 
-  const query: LocationQuery = route.query;
+	const query: LocationQuery = route.query;
 
-  const flowId = (query.flowId as LocationQueryValue) ?? "/";
-  queryParams.flowId=flowId;
+	const flowId = (query.flowId as LocationQueryValue) ?? "/";
+	queryParams.flowId = flowId;
 
 	loading.value = true;
 	queryFlowDataList(queryParams)
-		.then(({data}) => {
-      headList.value = data.headList;
-			roleList.value = data.records;
-			total.value = data.total;
-		})
-		.finally(() => {
-			loading.value = false;
-		});
-}
+			.then(({data}) => {
+				headList.value = data.headList;
+				roleList.value = []
 
+				var multipleIdArray = [];
+
+				for (var item of data.headList) {
+					if (item.type == 'SelectMultiUser' || item.type === 'UploadImage') {
+						multipleIdArray.push(item.id);
+					}
+				}
+				var dataRecordIndex = 0;
+				for (var item of data.records) {
+
+					var lengthArr = [1, 1];
+
+					for (var k of multipleIdArray) {
+						let itemElement = item[k];
+
+						if (isNotBlank(itemElement)) {
+							let length = JSON.parse(itemElement).length;
+							lengthArr.push(length);
+						}
+
+					}
+
+					let lcm = getLcmArray(lengthArr);
+
+					var lcmIndex = 0;
+
+					for (var l = 0; l < lcm; l++) {
+
+						var obj = {}
+
+						var spanColArray = [];
+
+
+						for (var head of data.headList) {
+							let val = item[head.id];
+							if (head.type == 'SelectMultiUser' || head.type === 'UploadImage') {
+
+								if (isNotBlank(val)) {
+									let jsonVal = JSON.parse(val);
+									let length = jsonVal.length;
+									//每个值占几个格子
+									let number = lcm / length;
+									obj[head.id] = jsonVal[Math.floor(lcmIndex / number)]
+									if (lcmIndex == 0) {
+										spanColArray.push({
+											rowspan: number,
+											colspan: 1
+										})
+									} else {
+										spanColArray.push({
+											rowspan: 0,
+											colspan: 0
+										})
+									}
+								} else {
+									obj[head.id] = {};
+									if (lcmIndex == 0) {
+										spanColArray.push({
+											rowspan: lcm,
+											colspan: 1
+										})
+									} else {
+										spanColArray.push({
+											rowspan: 0,
+											colspan: 0
+										})
+									}
+								}
+
+							} else {
+								obj[head.id] = val;
+
+								if (lcmIndex == 0) {
+									spanColArray.push({
+										rowspan: lcm,
+										colspan: 1
+									})
+								} else {
+									spanColArray.push({
+										rowspan: 0,
+										colspan: 0
+									})
+								}
+
+
+							}
+
+
+						}
+
+						spanArray.value[dataRecordIndex] = spanColArray
+
+
+						roleList.value.push(obj)
+						dataRecordIndex++;
+						lcmIndex++;
+					}
+				}
+				total.value = data.total;
+			})
+			.finally(() => {
+				loading.value = false;
+			});
+}
 
 
 onMounted(() => {
@@ -57,7 +159,17 @@ onMounted(() => {
 });
 
 
+const objectSpanMethod = ({
+														row,
+														column,
+														rowIndex,
+														columnIndex,
+													}) => {
 
+	let spanArrayElement = spanArray.value[rowIndex][columnIndex];
+
+	return spanArrayElement;
+}
 </script>
 
 <template>
@@ -72,66 +184,70 @@ onMounted(() => {
 					:data="roleList"
 					highlight-current-row
 					border
+					:span-method="objectSpanMethod"
 			>
-        <template  v-for="(item,index) in headList">
-          <template v-if="item.type==='SingleSelect'||item.type==='MultiSelect'">
-            <el-table-column  :label="item.name" :prop="item.id"  >
-              <template #default="scope">
-                {{JSON.parse(scope.row[item.id]).map(res=>res.value).join(",")}}
+				<template v-for="(item,index) in headList">
+					<template v-if="item.type==='SingleSelect'||item.type==='MultiSelect'">
+						<el-table-column :label="item.name" :prop="item.id">
+							<template #default="scope">
+								{{ JSON.parse(scope.row[item.id]).map(res => res.value).join(",") }}
 
-              </template>
-            </el-table-column>
+							</template>
+						</el-table-column>
 
-          </template>
-          <template v-else-if="item.type==='UploadImage'">
-            <el-table-column  :label="item.name" :prop="item.id"  >
-              <template #default="scope">
+					</template>
+					<template v-else-if="item.type==='UploadImage'">
+						<el-table-column :label="item.name" :prop="item.id">
+							<template #default="scope">
 
-                <el-image v-for="a in JSON.parse(scope.row[item.id])" style="width: 50px; height: 50px" :src="a.url" fit="contain" />
-              </template>
-            </el-table-column>
+								<el-image style="width: 50px; height: 50px" :src="scope.row[item.id].url"
+													fit="contain"/>
+							</template>
+						</el-table-column>
 
-          </template>
-          <template v-else-if="item.type==='UploadFile'">
-            <el-table-column  :label="item.name" :prop="item.id"  >
-              <template #default="scope">
+					</template>
+					<template v-else-if="item.type==='UploadFile'">
+						<el-table-column :label="item.name" :prop="item.id">
+							<template #default="scope">
 
-                <el-link v-for="a in JSON.parse(scope.row[item.id])" style="width: 50px; height: 50px" :href="a.url" target="_blank" >{{a.name}}</el-link>
-              </template>
-            </el-table-column>
+								<el-link v-for="a in JSON.parse(scope.row[item.id])" style="width: 50px; height: 50px" :href="a.url"
+												 target="_blank">{{ a.name }}
+								</el-link>
+							</template>
+						</el-table-column>
 
-          </template>
-          <template v-else-if="item.type==='Area'">
-            <el-table-column  :label="item.name" :prop="item.id" >
-              <template #default="scope">
- {{JSON.parse(scope.row[item.id]).name}}
-              </template>
-            </el-table-column>
+					</template>
+					<template v-else-if="item.type==='Area'">
+						<el-table-column :label="item.name" :prop="item.id">
+							<template #default="scope">
+								{{ JSON.parse(scope.row[item.id]).name }}
+							</template>
+						</el-table-column>
 
-          </template>
-          <template v-else-if="item.type==='SelectMultiUser'||item.type==='SelectMultiDept'||item.type==='SelectUser'||item.type==='SelectDept'">
-            <el-table-column  :label="item.name" :prop="item.id" >
-              <template #default="scope">
+					</template>
+					<template
+							v-else-if="item.type==='SelectMultiUser'||item.type==='SelectMultiDept'||item.type==='SelectUser'||item.type==='SelectDept'">
+						<el-table-column :label="item.name" :prop="item.id">
+							<template #default="scope">
+								<el-tag
+										style=" margin-right: 5px;margin-top: 5px;"
+										:key="scope.row[item.id].id"
+										:closable="false"
+										:type="scope.row[item.id].type==='dept'?'primary':(scope.row[item.id].type==='user'?'warning':'success')"
+										size="large"
+								>
+									{{ scope.row[item.id].name }}
+								</el-tag>
+							</template>
+						</el-table-column>
 
+					</template>
+					<template v-else>
+						<el-table-column :label="item.name" :prop="item.id" width="100"/>
 
-                <el-tag
-                    v-for="(item1, index) in JSON.parse(scope.row[item.id])" style=" margin-right: 5px;margin-top: 5px;"
-                    :key="item1.id"
-                    :closable="false"
-                    :type="item1.type==='dept'?'primary':(item1.type==='user'?'warning':'success')" size="large"
-                >
-                  {{ item1.name }}
-                </el-tag>
-              </template>
-            </el-table-column>
+					</template>
 
-          </template>
-          <template v-else>
-            <el-table-column  :label="item.name" :prop="item.id" width="100"/>
-
-          </template>
-
-        </template>
+				</template>
 
 
 			</el-table>
@@ -144,7 +260,6 @@ onMounted(() => {
 					@pagination="handleQuery"
 			/>
 		</el-card>
-
 
 
 	</div>
