fe893d0cde2b899d660049750be9d89cd46c67dc
zhaohuijun <18766289420@163.com>
2023-10-10 15:53:06 +0800

    feat:  优化文件表单上传显示

--- src/views/cxygzl/components/Upload/MultiUploadFile.vue
+++ src/views/cxygzl/components/Upload/MultiUploadFile.vue
@@ -1,18 +1,17 @@
-
 <template>
   <el-upload
-    v-model:file-list="fileList"
-    :before-upload="handleBeforeUpload"
-    :http-request="handleUpload"
-    :on-remove="handleRemove"
-    :on-preview="previewImg"
-    :limit="props.limit"
-    :disabled="disabled"
+      v-model:file-list="fileList"
+      :before-upload="handleBeforeUpload"
+      :http-request="handleUpload"
+      :on-remove="handleRemove"
+      :on-preview="previewImg"
+      :limit="props.limit"
+      :disabled="disabled"
   >
     <el-button type="primary">选择文件</el-button>
     <template #tip v-if="suffixArray?.length>0">
       <div class="el-upload__tip">
-        只支持文件格式：{{suffixArray.join(",")}}
+        只支持文件格式：{{ suffixArray.join(",") }}
       </div>
     </template>
   </el-upload>
@@ -28,7 +27,7 @@ import {
   UploadFile,
   UploadProps,
 } from "element-plus";
-import { uploadFileApi } from "../../api/file";
+import {uploadFileApi} from "../../api/file";
 
 const emit = defineEmits(["update:modelValue"]);
 
@@ -40,9 +39,9 @@ const props = defineProps({
   /**
    * 后缀
    */
-  suffixArray:{
-    type:Array,
-    default:()=>[]
+  suffixArray: {
+    type: Array,
+    default: () => []
   },
   /**
    * 文件路径集合
@@ -58,36 +57,54 @@ const props = defineProps({
     type: Number,
     default: 10,
   },
-	/**
-	 * 文件上传大小限制
-	 */
-	maxSize: {
-		type: Number,
-		default: 10,
-	},
+  /**
+   * 文件上传大小限制
+   */
+  maxSize: {
+    type: Number,
+    default: 10,
+  },
 });
 
 
 const fileList = ref([] as UploadUserFile[]);
 watch(
-  () => props.modelValue,
-  (newVal: string[]) => {
-    const filePaths = fileList.value.map((file) => file.url);
-    // 监听modelValue文件集合值未变化时，跳过赋值
-    if (
-      filePaths.length > 0 &&
-      filePaths.length === newVal.length &&
-      filePaths.every((x) => newVal.some((y) => y === x)) &&
-      newVal.every((y) => filePaths.some((x) => x === y))
-    ) {
-      return;
-    }
+    () => props.modelValue,
+    (newVal: string[]) => {
+      const filePaths = fileList.value.map((file) => file.url);
+      // 监听modelValue文件集合值未变化时，跳过赋值
+      if (
+          filePaths.length > 0 &&
+          filePaths.length === newVal.length &&
+          filePaths.every((x) => newVal.some((y) => y === x)) &&
+          newVal.every((y) => filePaths.some((x) => x === y))
+      ) {
+        return;
+      }
 
-    fileList.value = newVal.map((filePath) => {
-      return { url: filePath.url,name:filePath.name } as UploadUserFile;
-    });
-  },
-  { immediate: true }
+      console.log(newVal,fileList.value)
+      //判断url
+      if(fileList.value.length==newVal.length){
+        var match = true;
+        let urlArray1 = fileList.value.map(w => w.url).sort();
+        let urlArray2 = newVal.map(w => w.url).sort();
+        for (var k = 0; k < urlArray1.length; k++) {
+          if (urlArray1[k] != urlArray2[k]) {
+            match = false;
+            break
+          }
+        }
+        if (match) {
+          return;
+        }
+      }
+
+      console.log("文件列表", fileList.value, newVal)
+      fileList.value = newVal.map((filePath) => {
+        return {url: filePath.url, name: filePath.name} as UploadUserFile;
+      });
+    },
+    {immediate: true}
 );
 
 /**
@@ -96,27 +113,28 @@ watch(
  * @param params
  */
 async function handleUpload(options: UploadRequestOptions): Promise<any> {
-	console.log(options)
+
   // 上传API调用
-  const { data: fileInfo } = await uploadFileApi(options.file);
+  const {data: fileInfo} = await uploadFileApi(options.file);
 
   // 上传成功需手动替换文件路径为远程URL，否则图片地址为预览地址 blob:http://
   const fileIndex = fileList.value.findIndex(
-    (file) => file.uid == (options.file as any).uid
+      (file) => file.uid == (options.file as any).uid
   );
+  fileList.value[fileIndex].url = fileInfo;
 
-  fileList.value.splice(fileIndex, 1, {
-    name: options.file.name,
-    url: fileInfo,
-  }  );
+  // fileList.value.splice(fileIndex, 1, {
+  //   name: options.file.name,
+  //   url: fileInfo,
+  // }  );
 
   emit(
-    "update:modelValue",
-    fileList.value.map((file) => {
-		return {
-			url:file.url,name:file.name
-		}
-		})
+      "update:modelValue",
+      fileList.value.map((file) => {
+        return {
+          url: file.url, name: file.name
+        }
+      })
   );
 }
 
@@ -128,16 +146,16 @@ function handleRemove(removeFile: UploadFile) {
 
   if (filePath) {
     // deleteFileApi(filePath).then(() => {
-      // 删除成功回调
-      emit(
+    // 删除成功回调
+    emit(
         "update:modelValue",
         fileList.value.map((file) => {
-			return {
-		  url:file.url,name:file.name
+          return {
+            url: file.url, name: file.name
 
-			}
-				})
-      );
+          }
+        })
+    );
     // });
   }
 }
@@ -147,18 +165,18 @@ function handleRemove(removeFile: UploadFile) {
  */
 function handleBeforeUpload(file: UploadRawFile) {
 
-  console.log(file,props.suffixArray)
+  console.log(file, props.suffixArray)
 
   let name = file.name;
   var suffix = name.substring(name.lastIndexOf(".") + 1);
   if (props.suffixArray.indexOf(suffix) < 0) {
-    ElMessage.warning("表单不支持文件格式："+ suffix);
+    ElMessage.warning("表单不支持文件格式：" + suffix);
     return false
 
   }
 
   if (file.size > props.maxSize * 1048 * 1048) {
-    ElMessage.warning("上传文件不能大于"+props.maxSize+"M");
+    ElMessage.warning("上传文件不能大于" + props.maxSize + "M");
     return false;
   }
   return true;
