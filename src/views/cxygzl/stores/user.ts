5c1be15c58a90e65701fbab6b9204c6cf19b2bf7
zhaohuijun <zhaohuijun2021>
2023-11-23 23:09:42 +0800

    修复项目无法允许的bug

--- src/views/cxygzl/stores/user.ts
+++ src/views/cxygzl/stores/user.ts
@@ -3,7 +3,7 @@ import {defineStore} from "pinia";
 import {loginApi, loginByTokenApi, logoutApi} from "../api/auth";
 import {getUserInfo} from "../api/user";
 //TODO
-import {resetRouter} from "../router";
+import {resetRouter} from "@/router";
 import {store} from "./store.ts";
 
 import {LoginData} from "../api/auth/types";
